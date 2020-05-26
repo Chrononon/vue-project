ae76ad49074c03cd8a6a1b1779046c68dcf495a5
EgerieTH <bn.thanakit_st@tni.ac.th>
2020-05-26 17:20:50 +0700

    Create component tasklist(unfinished) and create to page

---
+++ src/components/TaskList.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class='contrainer'>
+    <div class='task-zone'>
+    <div class='drop-zone'> \
+        <h1>To-Do</h1>
+        <div class='drag-el'>Item 1</div>
+    </div>
+    <div class='drop-zone'> \
+        <h1>Doing</h1>
+        <div class='drag-el'>Item 2</div>
+    </div>
+    <div class='drop-zone'> \
+        <h1>Done</h1>
+        <div class='drag-el'>Item 3</div>
+    </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TaskList',
+    data(){
+        return{
+              task:[{
+                  id:1,
+                  title: 'Item A',
+                  status: 'todo'
+              },{
+                  id:2,
+                  title: 'Item B',
+                  status: 'todo'}
+                  ,{
+                  id:3,
+                  title: 'Item C',
+                  status: 'todo'}]
+        }
+        
+    }
+}
+</script>
+
+<style scoped>
+.contrainer{
+    margin: 30px 0;
+}
+.task-zone{
+    display: flex;
+    justify-content: space-around;
+}
+.drop-zone{
+    border: 1px solid black;
+    width: 250px;
+    height: 400px;
+    padding: 10px 0;
+}
+.drag-el{
+    border: 1px solid black;
+    width: 200px;
+    height: 40px;
+
+}
+</style>
